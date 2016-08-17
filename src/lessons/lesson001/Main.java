582633f92c23e745075d25d34a4d169c44f9033b
DrWorld <volshara2@gmail.com>
2016-08-17 19:14:20 +0300

    Init

---
+++ src/lessons/lesson001/Main.java
@@ -0,0 +1,10 @@
+package lessons.lesson001;
+
+/**
+ * Created by Holker on 17.08.2016.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello Denis");
+    }
+}
