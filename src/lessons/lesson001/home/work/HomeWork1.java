a55cf7fb43d6aadae0aef8b74d18d52525370247
DenisMD <markovdenis2012@gmail.com>
2016-08-17 21:21:47 +0500

    homeWork1

---
+++ src/lessons/lesson001/home/work/HomeWork1.java
@@ -0,0 +1,6 @@
+package lessons.lesson001.home.work;
+
+public class HomeWork1 {
+    //Домашние задание 1:
+    //Получить из файла Main.java исполняемый Byte-Code, как это сделать прочти в интернете
+}
