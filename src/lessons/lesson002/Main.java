323d12794c1977d17a854de4d3d47457cc3821dd
DenisMD <markovdenis2012@gmail.com>
2016-08-19 20:45:45 +0500

    Print

--- src/lessons/lesson002/Main.java
+++
@@ -1,7 +0,0 @@
-package lessons.lesson002;
-
-/**
- * Created by Holker on 17.08.2016.
- */
-public class Main {
-}
---
+++ src/lessons/lesson002/PrintMain.java
@@ -0,0 +1,25 @@
+package lessons.lesson002;
+
+//Станд. библиотека
+//--- I/O
+//Польз. библиотека
+
+//Вывод информации
+public class PrintMain {
+
+    //entry point
+    public static void main(String[] args) {
+        // Ctrl + /
+        // Ctrl + Shift + /
+        //        System.out.print("Test\n");
+        //        System.out.print("Test2");
+
+        //Ctrl + D
+        //        System.out.println("\tTest\tTest");
+        //        System.out.println("\tTest1\tTest");
+
+        System.err.println("Test");
+
+    }
+
+}
