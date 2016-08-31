3a87270acfae51d7379882bf273dd30960cb263e
dmarkov <markovdenis2012@gmail.com>
2016-08-31 09:42:20 +0500

    .

---
+++ src/lessons/lessons003/home/work/IntegerWork2.java
@@ -0,0 +1,13 @@
+package lessons.lessons003.home.work;
+
+//Найти путь пройденный автомобилем
+//Пользователь вводит скорость автомобиля (км/ч) - speed(v)
+//Пользователь вводит время в пути (ч) - time(t)
+//Напечатать путь пройденный автомобилем в км. - path(s)
+public class IntegerWork2 {
+
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ src/lessons/lessons003/home/work/IntegerWork5.java
@@ -0,0 +1,9 @@
+package lessons.lessons003.home.work;
+
+//Пользователь вводит количество астронамичесих едениц
+//Напечать сколько это будет километров
+
+//https://ru.wikipedia.org/wiki/%D0%90%D1%81%D1%82%D1%80%D0%BE%D0%BD%D0%BE%D0%BC%D0%B8%D1%87%D0%B5%D1%81%D0%BA%D0%B0%D1%8F_%D0%B5%D0%B4%D0%B8%D0%BD%D0%B8%D1%86%D0%B0
+//1 астраном. еденица = 149_597_870 км
+public class IntegerWork5 {
+}
