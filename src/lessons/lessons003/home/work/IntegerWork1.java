994ee69502f40723cacb3acf9ab92a749092ec1d
Fander <volshara2@gmail.com>
2016-09-18 15:28:21 +0300

    .

--- src/lessons/lessons003/home/work/IntegerWork1.java
+++ src/lessons/lessons003/home/work/IntegerWork1.java
@@ -9,6 +9,8 @@ package lessons.lessons003.home.work;
 //4) Целочисленное деление a на b
 //5) Деление с отстатком a на b
 
+import java.util.Scanner;
+
 //6) Изменить число a на 1, напечатать его\
 //7) Изменить число b на -1, напечатать его
 //8) Изменить чсилоа a на (011011 - число в двоичной системе счисления), напечатать его
@@ -16,7 +18,23 @@ package lessons.lessons003.home.work;
 public class IntegerWork1 {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner (System.in);
+        int a ;
+        int b ;
+        System.out.println("Введите 1 число");
+        a = scanner.nextInt() ;
+        System.out.println("Введите 2 число");
+        b = scanner.nextInt() ;
+        a -= a;
+        a++;
+        b -= b;
+        b--;
+        System.out.println(a +" " + b);
 
+        a = a = 0x011011;
+        b = b = 0xFA50;
+        System.out.println(a);
+        System.out.println(b);
     }
 
 }
