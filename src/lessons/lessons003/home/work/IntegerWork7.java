6845215d895b771200c20633df3d13a602b10dfe
Fander <volshara2@gmail.com>
2016-10-01 16:17:58 +0300

    .

--- src/lessons/lessons003/home/work/IntegerWork7.java
+++ src/lessons/lessons003/home/work/IntegerWork7.java
@@ -1,8 +1,25 @@
 package lessons.lessons003.home.work;
 
+import java.util.Scanner;
+
 //Пользователь вводит общее количество байт
 //Напечатать сколько это мегабайт, килобайт и оставшихся байт
 // 1 Кбайт - 1024 байт
 // 1 Мбайт - 1024 Кбайт
 public class IntegerWork7 {
+    public static void main(String[] args) {
+        long bytes;
+        long kbytes;
+        long mbytes;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите количество байт");
+        bytes = scanner.nextInt();
+        mbytes = bytes / (1024*1024);
+        bytes = bytes % (1024*1024);
+        kbytes = bytes / 1024 ;
+        bytes = bytes % 1024;
+
+        System.out.println("Вот сколько получается = " + mbytes + " Мбайт " + kbytes + "  Кбайт " + " байт осталась " );
+
+    }
 }
--- src/lessons/lessons003/home/work/IntegerWork8.java
+++ src/lessons/lessons003/home/work/IntegerWork8.java
@@ -8,6 +8,37 @@ package lessons.lessons003.home.work;
 //1) Пользователь вводит координаты левого нижнего угла (x1,y1)
 //2) Пользователь вводит координаты верхнего правого угла (x2,y2)
 
+import java.util.Scanner;
+
 //Вывести значение площади прямоугольника
 public class IntegerWork8 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int ax;
+        int ay;
+        int bx;
+        int by;
+        long x;
+        long y;
+        long pr; //переметр
+        long pl; // площадь
+        System.out.println("Введите кардинату x cтороны a");
+
+        ax = scanner.nextInt();
+        System.out.println("Введите кардинату y cтороны a");
+        ay = scanner.nextInt();
+        System.out.println("Введите кардинату x cтороны b");
+        bx = scanner.nextInt();
+        System.out.println("Введите кардинату y cтороны b");
+        by = scanner.nextInt();
+
+        x = ax - bx;
+        y = ay - by;
+        pl = x * y;
+        pr =  ((x + y) * 2);
+        System.out.println("Сторона x " + x);
+        System.out.println("сторона y " + y);
+        System.out.println("Площадь " + pl);
+        System.out.println("Переметр " + pr );
+    }
 }
