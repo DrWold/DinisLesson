36c14df1f3a576b8b3ed8ae127f3385cb475b8d5
Holder <volshara2@gmail.com>
2016-11-13 16:33:12 +0300

    .

--- src/lessons/lessons005/home/work/LogicHomeWork1.java
+++ src/lessons/lessons005/home/work/LogicHomeWork1.java
@@ -1,10 +1,27 @@
 package lessons.lessons005.home.work;
 
 
+import java.util.Scanner;
+
 // Пользователь вводит с клавиатуры тумпературу в градусах, это целое число число
 // Создать 3 логических условий и вывсети их резултат
 // 1) Температура меньше абсолютного минуса (-270°)
 // 2) Температура больше абсолютного минуса но меньше или равно нулю
 // 3) Температура больше 0 и меньше 5000°
 public class LogicHomeWork1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner (System.in);
+        int ABSOLUTE_TEMP = -270;
+        int temp; // температура в цельсии
+        System.out.println("Ввидите температуру в цельсии ");
+        temp = scanner.nextInt();
+
+        boolean logic1, logic2, logic3;
+        logic1 = ABSOLUTE_TEMP > temp;
+        logic2 = ABSOLUTE_TEMP < temp && temp <= 0;
+        logic3 = temp > 0 && temp < 5000;
+
+        System.out.println(logic1 + " " + logic2 + " " +  logic3);
+
+    }
 }
--- src/lessons/lessons005/home/work/LogicHomeWork2.java
+++ src/lessons/lessons005/home/work/LogicHomeWork2.java
@@ -9,4 +9,51 @@ package lessons.lessons005.home.work;
 // 2) Стоимость квартиры от 1000_000 до 3000_000 и квартира не на первом этаже
 // 3) Количество комнат больше 3 или стоимость квартиры меньше 1000_000
 public class LogicHomeWork2 {
+    public static void main(String[] args) {
+        int price1, countRoom1, floor1;
+        int price2, countRoom2, floor2;
+        int price3, countRoom3, floor3;
+
+        price1 = 5000;
+        countRoom1 = 2;
+        floor1 = 1;
+
+        price2 = 3000;
+        countRoom2 = 3;
+        floor2 = 4 ;
+
+        price3 = 7000;
+        countRoom3 = 6 ;
+        floor3 = 5;
+
+        boolean logic1_1, logic1_2, logic1_3;
+        logic1_1 = floor1 >= 5 && floor1 <= 9 && countRoom1 == 2;
+        logic1_2 = floor2 >= 5 && floor2 <= 9 && countRoom2 == 2;
+        logic1_3 = floor3 >= 5 && floor3 <= 9 && countRoom3 == 2;
+        System.out.println("Проверяется условие что квартиры между 5 и 9 этажем двух комнатные: ");
+        System.out.println("Квартира 1: " + logic1_1);
+        System.out.println("Квартира 2: " + logic1_2);
+        System.out.println("Квартира 3: " + logic1_3);
+
+        boolean logic2_1, logic2_2, logic2_3;
+        logic2_1 = price1 >= 1000_000 && price1 <= 3000_000 && floor1 != 1;
+        logic2_2 = price2 >= 1000_000 && price2 <= 3000_000 && floor2 != 1;
+        logic2_3 = price3 >= 1000_000 && price3 <= 3000_000 && floor3 != 1;
+
+        System.out.println("Проверяем условие что квартиры стоят от 1000_000 до 3000_000 и не на первой этаже");
+        System.out.println("Квартира 1 " + logic2_1);
+        System.out.println("Квартира 2 " + logic2_2);
+        System.out.println("Квартира 3 " + logic2_3);
+
+        boolean logic3_1, logic3_2, logic3_3;
+        logic3_1 = price1 <= 1000_000 || countRoom1 > 3;
+        logic3_2 = price2 <= 1000_000 || countRoom2 > 3;
+        logic3_3 = price3 <= 1000_000 || countRoom3 > 3;
+
+        System.out.println("Проверяем условие что квартиры стоят меньше одного миллиона или больше трех комнат");
+        System.out.println("Квартира 1 " + logic3_1);
+        System.out.println("Квартира 2 " + logic3_2);
+        System.out.println("Квартира 3 " + logic3_3);
+
+    }
 }
