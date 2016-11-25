aa4c6e295a62c3b4e3d2fdee8814f8604025bd2a
DenisMD <markovdenis2012@gmail.com>
2016-11-25 20:12:51 +0500

    .

--- src/lessons/lessons006/home/work/IfWork1.java
+++ src/lessons/lessons006/home/work/IfWork1.java
@@ -13,33 +13,37 @@ import java.util.Scanner;
 //сделать 2 реализации с помощью if и с помощью тернарного оператора (?:)
 public class IfWork1 {
     public static void main(String[] args) {
-        int numberOne;
-        int numberDuo;
+
+        Scanner scanner = new Scanner(System.in);
+
+        int number1;
+        int number2;
         int numberMax;
         int numberMin;
         int numberSmooth = 4;
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите 1 число");
-        numberOne = scanner.nextInt();
-        System.out.println("Введите 2 число");
-        numberDuo = scanner.nextInt();
 
-        if(numberOne > numberDuo) {
-            System.out.println("число " + numberOne + " больше чем число " + numberDuo );
-        }else if (numberOne == numberDuo){
-            System.out.println( " вы ввели 1 и тоже число " + numberOne );
-        }else {
-            System.out.println("число " + numberDuo + " больше чем число " + numberOne);
+        System.out.println("Введите 1 число");
+        number1 = scanner.nextInt();
+        System.out.println("Введите 2 число");
+        number2 = scanner.nextInt();
+
+        if (number2 == number1) {
+            System.out.println( "Вы ввели 1 и тоже число " + number1 );
+            return;
         }
 
-        numberMax = (numberOne > numberDuo ? numberOne : numberDuo);
+        if(number1 > number2) {
+            System.out.println("число " + number1 + " больше чем число " + number2 );
+        } else {
+            System.out.println("число " + number2 + " больше чем число " + number1);
+        }
 
-        numberMin = (numberOne < numberDuo ? numberOne : numberDuo);
-        System.out.println(numberMax + " больше ");
-        System.out.println(numberMin + " меньше ");
-        System.out.println(numberSmooth  + " вы ввели 1 и тоже число ");
+        numberMax = (number1 > number2 ? number1 : number2);
+        numberMin = (number1 < number2 ? number1 : number2);
 
-
+        System.out.println(numberMax + " больше");
+        System.out.println(numberMin + " меньше");
+        System.out.println(numberSmooth  + " вы ввели 1 и тоже число");
 
     }
 }
