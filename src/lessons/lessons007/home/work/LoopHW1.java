928b9d12a01b7c150f565e60b20d43161c9aa300
Holder <volshara2@gmail.com>
2016-11-26 21:53:21 +0300

    ,

--- src/lessons/lessons007/home/work/LoopHW1.java
+++ src/lessons/lessons007/home/work/LoopHW1.java
@@ -10,4 +10,15 @@ package lessons.lessons007.home.work;
 // 300
 // Сделать 2 реализации, через for и через while
 public class LoopHW1 {
+    public static void main(String[] args) {
+       int i = 60;
+        while (i < 300){
+            i+=5;
+            System.out.println(i);
+        }
+       // for (int i=60; i < 300; i+=5) {
+         //   System.out.println(i);
+
+        //}
+    }
 }
--- src/lessons/lessons007/home/work/LoopHW2.java
+++ src/lessons/lessons007/home/work/LoopHW2.java
@@ -10,4 +10,15 @@ package lessons.lessons007.home.work;
 // 60
 // Сделать 2 реализации, через for и через while
 public class LoopHW2 {
+    public static void main(String[] args) {
+        //int i = 300;
+        //while (i >= 60) {
+          //  i -= 5;
+        //    System.out.println(i);
+        //}
+         for (int i=300; i > 60; i-=5) {
+           System.out.println(i);
+
+        }
+    }
 }
