14f7b0b55eaec717f12022ab9492603b84d1c159
Holder <volshara2@gmail.com>
2016-11-27 19:56:10 +0300

    r

--- src/lessons/lessons007/home/work/LoopHW6.java
+++ src/lessons/lessons007/home/work/LoopHW6.java
@@ -1,5 +1,8 @@
 package lessons.lessons007.home.work;
 
+import java.util.Random;
+import java.util.Scanner;
+
 // Игра казино-автомат
 // У пользователя 1000 рублей
 // Пользователю предлагается сделать ход, он должен выбрать Y или N
@@ -10,4 +13,31 @@ package lessons.lessons007.home.work;
 // Если в числе последняя цифра 5, он выигрывает 20 рублей(чтобы узнать посл. цифру чимсла можно разделить с остатком на 10 : 234 % 10 = 4)
 // Иначе он проиграл
 public class LoopHW6 {
+    public static void main(String[] args) {
+        Random random = new Random();
+        Scanner scanner = new Scanner(System.in);
+
+        int balance = 1000;
+        char answer;
+        do {
+
+            int kazino;;
+            System.out.println("Здраствуйте чтоб сыграть в казино нажмите Y|N");
+            System.out.println("ВНИМАНИЕ ИГРА СТОИТ 5 МАНЕТ У ВАС : " + balance);
+            answer = scanner.next().charAt(0);
+            balance -= 5;
+            kazino = random.nextInt(900) + 100 ;
+
+            if (kazino == 111 || kazino == 222 || kazino == 333 || kazino == 444 || kazino == 555 || kazino == 666 || kazino == 777 || kazino == 888 || kazino == 999 ){
+                System.out.println("вы выйграли Джекпот" + kazino);
+                balance += 100;
+            }
+
+
+
+        } while (answer == 'Y' || answer == 'y');
+
+
+
+    }
 }
