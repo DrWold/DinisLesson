923b1cf95359566b68a4b564ac56116c3c18682f
Holder <volshara2@gmail.com>
2016-11-27 18:57:33 +0300

    r

--- src/lessons/lessons007/home/work/LoopHW5.java
+++ src/lessons/lessons007/home/work/LoopHW5.java
@@ -12,4 +12,22 @@ package lessons.lessons007.home.work;
 //**
 //*
 public class LoopHW5 {
+    public static void main(String[] args) {
+//        for (int i = 0; i < 10; i++) {
+//            if (i == 9) {
+//                break;
+//            }
+//            for (int j = 0; j <= i; j++) {
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//        }
+        for (int i = 0; i < 10; i++) {
+
+            for (int j = 10; j > i; j--) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
 }
