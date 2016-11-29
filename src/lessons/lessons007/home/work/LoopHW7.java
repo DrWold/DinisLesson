c6b61a228d732b9c82ba611ce87b18b4045c2a37
Holder <volshara2@gmail.com>
2016-11-29 16:08:22 +0300

    ,

--- src/lessons/lessons007/home/work/LoopHW7.java
+++ src/lessons/lessons007/home/work/LoopHW7.java
@@ -29,12 +29,53 @@ public class LoopHW7 {
         int demonHp = 500;
         int heroHp = 100;
         char answer;
+        int answer2;// ответ действия
+
+        ////////////////////////////////////////////////////
+        int damageHero ;
+        int damagedemon = random.nextInt(30) + 10  ;
+
+
+
+        ////////////////////////////////////////////////////
 
 
         System.out.println("|----------------------------------------------------------|");
         System.out.println("|                    Герой против Демона                   |");
         System.out.println("|--------------------------------------------------------- |");
         System.out.println("Чтоб начать игру нажмите Y/N");
+        answer = scanner.next().charAt(0);
+        if (answer != 'Y' && answer != 'y' ){
+            return;
+        }
+        do {
 
+            System.out.println("");
+            System.out.println("Выберите действие ");
+            System.out.println("1.Узнать свое здоровье");
+            System.out.println("2.Узнать здоровье демона");
+            System.out.println("3.Выпить зелье ");
+            System.out.println("4. Ударить демона ");
+            System.out.println("5. Сделать кувырок ");
+            System.out.println("6. Поставить щит ");
+            System.out.println("7. Убежать ");
+            System.out.println("8. Предложить выпить (вероятность убить мгновенно демона)");
+
+            answer2 = scanner.nextInt();
+            if (answer2 == 1){
+                System.out.println("у вас : " + heroHp);
+            }else if (answer2 == 2){
+                System.out.println("У вас : " + demonHp);
+            }else if (answer2 == 3){
+                System.out.println("Гобби");
+            }else if (answer2 == 4){
+                damageHero = random.nextInt(40) + 10 ;
+                System.out.println("Вы ударили демона на " + damageHero  );
+                demonHp -= damageHero;
+                System.out.println("и получили в ответ " + damagedemon);
+                heroHp -= damagedemon;
+
+            }
+        }while (true);
     }
 }
