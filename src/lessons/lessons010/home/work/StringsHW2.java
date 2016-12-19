d4e74867cc2f9ba9a3e32d3fbc23e709071584c5
Gordon <volshara2@gmail.com>
2016-12-19 20:28:16 +0300

    Выполненно #LESSON-4 Fixed

---
+++ src/lessons/lessons010/home/work/StringsHW2.java
@@ -0,0 +1,27 @@
+package lessons.lessons010.home.work;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+//Пользователь вводит строку:
+//        1) Найти в строке все слова "fuck" и заменить их на "f!#k"
+//        2) Удалить из строки нецензурные слова(какие слова придумай сам)
+public class StringsHW2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите текст");
+        String text = scanner.nextLine();
+        String[] disros = new String[] {"fuck", "sick", "bitch"};
+        String[] words = text.split(" ");
+        String result = "";
+        for (String word : words) {
+            for (String vords :disros )
+            if (word.equals(vords)) {
+                word = "****";
+            }
+            result +=  word + ' ';
+        }
+        System.out.println(result);
+
+    }
+}
