dd02ce5f30994a87d9917f60258635e14fd4d989
Gordon <volshara2@gmail.com>
2016-12-20 19:00:17 +0300

    Выполненно #LESSONS-6 Fixed

---
+++ src/lessons/lessons010/home/work/StringsHW3.java
@@ -0,0 +1,13 @@
+package lessons.lessons010.home.work;
+
+/**
+ * Created by Gordon on 20.12.2016.
+ */
+public class StringsHW3 {
+    public static void main(String[] args) {
+        String text = "Hello word . Test test";
+
+
+        System.out.println(text.substring(4, 14));
+    }
+}
